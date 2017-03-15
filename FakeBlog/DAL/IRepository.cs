961bbb6c3f84b76fc25372f02e71cab6df9befdc
abbie <abigail.c.holder@gmail.com>
2017-03-14 21:17:11 -0500

    Setting up fake blog

---
+++ FakeBlog/DAL/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using FakeBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeBlog.DAL
+{
+    interface IRepository
+    {
+        //Create
+        void AddPost(string name, Author author);
+        void AddPost(string name, int postId);
+
+        //Read
+        Post GetPost(int postId);
+
+        //Update
+        bool AttachUser(string userId, int postId); // true: successful, false: not successful
+
+        //Delete
+        bool RemovePost(int postId);
+    }
+}
